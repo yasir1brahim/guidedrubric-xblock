eab1cb55530f655a28301dbde2b159c29590caba
Yasir Ibrahim <yasir.ibrahim@gmail.com>
2024-03-22 09:52:00 +0500

    Converted Guided Rubric Code As Per XBlock structure Added JS to call ajax and type message to UI Added basic user interface for XBlock screen Added loader

--- bin/guidedrubric/guidedrubric/guidedrubric.py
+++ bin/guidedrubric/guidedrubric/guidedrubric.py
@@ -4,7 +4,364 @@ import pkg_resources
 from web_fragments.fragment import Fragment
 from xblock.core import XBlock
 from xblock.fields import Integer, Scope
+import openai
+from dotenv import load_dotenv
+import time
+import re
+import json
 
+
+load_dotenv()
+client = openai.OpenAI()
+model = "gpt-4-turbo-preview"
+
+
+phases = [
+    {
+        "id": "name",
+        "question": """What is your name?""",
+        "sample_answer": "",
+        "instructions": """The user will give you their name. Then, welcome the user to the exercise, and explain that you'll help them and provide feedback as they go. End your statement with "I will now give you your first question about the article." """,
+        "rubric": """
+            1. Name
+                    1 point - The user has provided a response in this thread. 
+                    0 points - The user has not provided a response. 
+        """,
+        "label": "GO!",
+        "minimum_score": 0
+    },
+    {
+        "id": "about",
+        "question": """What is the article about?""",
+        "sample_answer":"This article investigates the impact of various video production decisions on student engagement in online educational videos, utilizing data from 6.9 million video watching sessions on the edX platform. It identifies factors such as video length, presentation style, and speaking speed that influence engagement, and offers recommendations for creating more effective educational content.",
+        "instructions": "Provide helpful feedback for the following question. If the student has not answered the question accurately, then do not provide the correct answer for the student. Instead, use evidence from the article coach them towards the correct answer. If the student has answered the question correctly, then explain why they were correct and use evidence from the article. Question:",
+        "rubric": """
+                1. Length
+                    1 point - Response is greater than or equal to 150 characters.
+                    0 points - Response is less than 150 characters. 
+                2. Key Points
+                    2 points - The response mentions both videos AND student engagement rates
+                    1 point - The response mentions either videos OR student engagement rates, but not both
+                    0 points - The response does not summarize any important points in the article. 
+        """,
+        "minimum_score": 2
+    },
+    {
+       "id": "methdologies",
+       "question": "Summarize the methodology(s) used.",
+       "sample_answer": "The study gathered data around video watch duration and problem attempts from the edX logs. These metrics served as a proxy for engagement. Then it compared that with video attributes like length, speaking rate, type, and production style, to determine how video production affects engagement.",
+       "instructions": "Provide helpful feedback for the following question. If the student has not answered the question accurately, then do not provide the correct answer for the student. Instead, use evidence from the article coach them towards the correct answer. If the student has answered the question correctly, then explain why they were correct and use evidence from the article. Question:",
+       "rubric": """
+               1. Correctness
+                   1 point - Response is correct and based on facts in the paper
+                   0 points - Response is incorrect or not based on facts in the paper
+               """,
+       "minimum_score": 1
+    },
+    {
+        "id": "findings",
+        "question": "What were the main findings in the article?",
+        "sample_answer": "Shorter videos are more engaging; Faster-speaking instructors hold students' attention better; High production value does not necessarily correlate with higher engagement;",
+        "instructions": "Provide helpful feedback for the following question. If the student has not answered the question accurately, then do not provide the correct answer for the student. Instead, use evidence from the article coach them towards the correct answer. If the student has answered the question correctly, then explain why they were correct and use evidence from the article. Question:",
+        "rubric": """
+            1. Correctness
+                    2 points - Response includes two or more findings or recommendations from the study
+                    1 point - Response includes only one finding or recommendation form the study
+                    0 points - Response includes no findings or recommendations or is not based on facts in the paper
+                    """,
+        "minimum_score": 1
+    },
+    {
+        "id": "limitations",
+        "question": "What are some of the weaknesses of this study?",
+        "sample_answer": "The study cannot measure true student engagement, and so it must use proxies; The study could not track any offline video viewing; The study only used data from math/science courses;",
+        "instructions": "Provide helpful feedback for the following question. If the student has not answered the question accurately, then do not provide the correct answer for the student. Instead, use evidence from the article coach them towards the correct answer. If the student has answered the question correctly, then explain why they were correct and use evidence from the article. Question:",
+        "rubric": """
+            1. Correctness
+                    2 points - Response includes two or more limitations of the study
+                    1 point - Response includes only one limitation in the study
+                    0 points - Response includes no limitations or is not based on facts in the paper
+                2. Total Score
+                    The total sum of their scores. 
+            """,
+        "minimum_score": 1
+    }
+    #Add more steps as needed
+    
+]
+
+session_state = {
+    "current_question_index": 0,
+    "thread_obj": None,
+    # Add more session state variables as needed
+}
+
+current_question_index = session_state['current_question_index'] if 'current_question_index' in session_state else 0
+
+class AssistantManager:
+    thread_id = ""
+    assistant_id = "asst_BFlYa2t1svtMFaGbMkB4QuMp"
+
+
+    if 'current_question_index' not in session_state:
+        session_state.thread_obj = []
+
+
+    def __init__(self, model: str = model):
+        self.client = client
+        self.model = model
+        self.assistant = None
+        self.thread = None
+        self.run = None
+        self.summary = None
+
+        # Retrieve existing assistant and thread if IDs are already set
+        if AssistantManager.assistant_id:
+            self.assistant = self.client.beta.assistants.retrieve(
+                assistant_id=AssistantManager.assistant_id
+            )
+        if AssistantManager.thread_id:
+            self.thread = self.client.beta.threads.retrieve(
+                thread_id=AssistantManager.thread_id
+            )
+
+    def create_assistant(self, name, instructions, tools):
+        if not self.assistant:
+            assistant_obj = self.client.beta.assistants.create(
+                name=name, instructions=instructions, tools=tools, model=self.model
+            )
+            AssistantManager.assistant_id = assistant_obj.id
+            self.assistant = assistant_obj
+            print(f"AssisID:::: {self.assistant.id}")
+
+    def create_thread(self):
+        if not self.thread:
+            if session_state['thread_obj']:
+                print(f"Grabbing existing thread...")
+                thread_obj = session_state['thread_obj']
+            else:
+                print(f"Creating and saving new thread")
+                thread_obj = self.client.beta.threads.create()
+                session_state['thread_obj'] = thread_obj
+
+            AssistantManager.thread_id = thread_obj.id
+            self.thread = thread_obj
+            print(f"ThreadID::: {self.thread.id}")
+
+    def add_message_to_thread(self, role, content):
+        if self.thread:
+            self.client.beta.threads.messages.create(
+                thread_id=self.thread.id, role=role, content=content
+            )
+
+    def run_assistant(self, instructions):
+        if self.thread and self.assistant:
+            self.run = self.client.beta.threads.runs.create(
+                thread_id=self.thread.id,
+                assistant_id=self.assistant.id,
+                instructions=instructions,
+            )
+
+    def process_message(self):
+        if self.thread:
+            messages = self.client.beta.threads.messages.list(thread_id=self.thread.id)
+            summary = []
+
+            last_message = messages.data[0]
+            role = last_message.role
+            response = last_message.content[0].text.value
+            summary.append(response)
+
+            self.summary = "\n".join(summary)
+            
+    def call_required_functions(self, required_actions):
+        if not self.run:
+            return
+        tool_outputs = []
+
+        for action in required_actions["tool_calls"]:
+            func_name = action["function"]["name"]
+            arguments = json.loads(action["function"]["arguments"])
+
+            if func_name == "respond":
+                output = respond(structured_response=arguments["structured_response"])
+            
+                final_str = ""
+                for item in output:
+                    final_str += "".join(item)
+
+                tool_outputs.append({"tool_call_id": action["id"], "output": final_str})
+            else:
+                raise ValueError(f"Unknown function: {func_name}")
+
+        print("Submitting outputs back to the Assistant...")
+        self.client.beta.threads.runs.submit_tool_outputs(
+            thread_id=self.thread.id, run_id=self.run.id, tool_outputs=tool_outputs
+        )
+
+    def get_summary(self):
+        return self.summary
+
+    def wait_for_completion(self):
+        if self.thread and self.run:
+            while True:
+                time.sleep(5)
+                run_status = self.client.beta.threads.runs.retrieve(
+                    thread_id=self.thread.id, run_id=self.run.id
+                )
+                # print(f"RUN STATUS:: {run_status.model_dump_json(indent=4)}")
+
+                if run_status.status == "completed":
+                    self.process_message()
+                    break
+                elif run_status.status == "requires_action":
+                    print("FUNCTION CALLING NOW...")
+                    self.call_required_functions(
+                        required_actions=run_status.required_action.submit_tool_outputs.model_dump()
+                    )
+
+
+def extract_score(text):
+    # Define the regular expression pattern
+    #regex has been modified to grab the total value whether or not it is returned inside double quotes. The AI seems to fluctuate between using quotes around values and not. 
+    pattern = r'"total":\s*"?(\d+)"?'
+    
+    # Use regex to find the score pattern in the text
+    match = re.search(pattern, text)
+    
+    # If a match is found, return the score, otherwise return None
+    if match:
+        return int(match.group(1))
+    else:
+        return 0
+
+def check_score(score, question_num):
+    if score >= phases[question_num]["minimum_score"]:
+        return True
+    else:
+        return False
+
+def handle_skip(index):
+    session_state[f"phase_{index}_state"] = "skip"
+    session_state.current_question_index += 1
+
+def build_instructions(index, graded_step=False):
+    if graded_step:
+        compiled_instructions = """Please provide a score for the previous user message in this thread. Use the following rubric:
+        """ + phases[index]["rubric"] + """
+        Please output your response as JSON, using this format: { "[criteria 1]": "[score 1]", "[criteria 2]": "[score 2]", "total": "[total score]" }"""
+    else:
+        compiled_instructions = phases[index]["instructions"] + phases[index]["question"]
+
+    return compiled_instructions
+
+def handle_assistant_grading(index, manager):
+
+    instructions = build_instructions(index, True)
+    manager.run_assistant(instructions)
+    manager.wait_for_completion()
+
+    #get the score summary
+    summary = "SCORE: " + manager.get_summary()
+    #save the score summary
+    session_state[f"phase_{index}_rubric"] = summary
+
+    print(f"RUBRIC SCORE: {summary}")
+
+    #Extract the numeric score from the json
+    score = extract_score(str(summary))
+    #save the numeric score
+    session_state[f"phase_{index}_score"] = score
+    
+    #If the score passes, then increase the index to move to the next step                
+    if check_score(score, index):
+        session_state[f"phase_{index}_state"] = "pass"
+        session_state['current_question_index'] += 1
+        print(session_state['current_question_index'])
+    else:
+        session_state['current_question_index'] += 1
+        session_state[f"phase_{index}_state"] = "fail"
+
+def handle_assistant_interaction(index, manager, user_input):
+    #hide the buttons
+    manager.add_message_to_thread(
+        role="user", content=user_input
+    )
+    instructions = build_instructions(index)
+    manager.run_assistant(instructions)
+
+    manager.wait_for_completion()
+
+    #get the AI Feedback
+    summary = manager.get_summary()
+    #save the AI Feedback
+    session_state[f"phase_{index}_summary"] = summary
+    #write the AI feedback
+    print(summary)
+    return summary
+
+def main(user_input):
+
+    print('Guided Critical Analysis')
+    print('In this guided article review, we\'ll both read the same journal article. Then, you\'ll be guided through an analysis of the paper. Let\'s begin by reading the paper!')
+
+    print("View PDF", "http://up.csail.mit.edu/other-pubs/las2014-pguo-engagement.pdf")
+
+    print("""
+        This is a **DEMO**, so sample answers are **pre-filled**""")
+
+    #Create the assistant one time. Only if the Assistant ID is not found, create a new one. 
+    manager = AssistantManager()
+
+    manager.create_assistant(
+    name="Guided Rubric",
+    instructions="""You are a helpful tutor that is guiding a university student through a critical appraisal of a scholarly journal article. You want to encourage the students ideas, but you also want those idea to be rooted in evidence from the journal article that you'll fetch via retrieval. 
+
+Generally, you will be asked to provided feedback on the students answer based on the article, and you'll also sometimes be asked to score the submission based on a rubric which will be provided. More specific instructions will be given in the instructions via the API. 
+        """,
+    tools=""
+    )
+    manager.create_thread()
+  
+    i=0
+    if  session_state['current_question_index'] <= len(phases) - 1:
+        i = session_state['current_question_index']
+        index = i
+
+        # if i == 0:
+        #     user_input = "testuser"
+        # else:
+        #     # if i > len(phases) - 1:
+        #     #     break
+        #     user_input = phases[index]["sample_answer"]
+
+        if f"phase_{index}_summary" in session_state:
+            stored_summary = session_state[f"phase_{index}_summary"]
+            print(stored_summary)
+
+        #if we've reached the final question and outputted, then end. 
+        if session_state['current_question_index'] == len(phases) and i == session_state['current_question_index'] - 1:
+            
+            print("You've reached the end of the exercise. Hope you learned something!")
+            return
+        
+        if i <= len(phases)-1:
+            if i == session_state['current_question_index']:
+                submit_button = True
+        else:
+            print("You've reached the end!")
+
+        i+=1
+
+        if session_state['current_question_index'] == len(phases):
+            return
+
+        if session_state['current_question_index'] <= len(phases):
+            if submit_button:
+                hand_intr = handle_assistant_interaction(index, manager, user_input)
+                hand_gra = handle_assistant_grading(index, manager)
+        # question = phases[session_state['current_question_index']]['question'] if 'question' in phases[session_state['current_question_index']] and phases[session_state['current_question_index']]['question'] else None
+        return hand_intr, hand_gra, 
 
 class GuidedRubricXBlock(XBlock):
     """
@@ -41,15 +398,12 @@ class GuidedRubricXBlock(XBlock):
     # TO-DO: change this handler to perform your own actions.  You may need more
     # than one handler, or you may not need any handlers at all.
     @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
-        """
-        An example handler, which increments the data.
-        """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
-
-        self.count += 1
-        return {"count": self.count}
+    def send_message(self, data, suffix=""):
+        """Send message to OpenAI, and return the response"""
+        user_input = data['message']
+        res = main(user_input)
+        print(res)
+        return {'result': 'success' if res else 'failed', 'response': res}
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
--- bin/guidedrubric/guidedrubric/static/js/src/guidedrubric.js
+++ bin/guidedrubric/guidedrubric/static/js/src/guidedrubric.js
@@ -1,22 +1,86 @@
 /* Javascript for GuidedRubricXBlock. */
 function GuidedRubricXBlock(runtime, element) {
 
-    function updateCount(result) {
-        $('.count', element).text(result.count);
-    }
+    let chatMsg = document.getElementById('chat-msg');
+    let chatLogs = document.getElementById('chat-logs');
+    let sendBtn = document.getElementById('send-btn');
+    let errorMsg = document.getElementById('error-msg');
+    let loadingMsg = document.createElement('div');
+    var overlay = document.createElement("div");
+    overlay.setAttribute("class", "overlay");
+    overlay.setAttribute("id", "overlay-loader");
+    var loader = document.createElement("div");
+    loader.setAttribute("class", "lds-dual-ring");
+    overlay.appendChild(loader);
+    loadingMsg.classList.add("loading");
+    loadingMsg.appendChild(loader);
+    // Handlers
+    var handlerUrl = runtime.handlerUrl(element, 'send_message');
 
-    var handlerUrl = runtime.handlerUrl(element, 'increment_count');
-
-    $('p', element).click(function(eventObject) {
-        $.ajax({
+    function send_message(message) {
+        return $.ajax({
             type: "POST",
             url: handlerUrl,
-            data: JSON.stringify({"hello": "world"}),
-            success: updateCount
+            data: JSON.stringify({"message": message}),
+            contentType:"application/json; charset=utf-8",
+            dataType: "json",
+        }).done(function(response) {
+            if (response.result === 'success') {
+                chatLogs.removeChild(loadingMsg);
+                console.log(response.response);
+                type_message(response.response);
+            } else {
+                alert("You've reached the end of the exercise. Hope you learned something!");
+                chatLogs.removeChild(loadingMsg);
+                document.querySelector('.chat-input').style.display = 'block';
+            }
+        }).fail(function(error) {
+            console.log("An error occurred: ", error);
         });
+    };
+
+    sendBtn.addEventListener('click', function() {
+        if (!chatMsg.value.trim()) {
+            errorMsg.textContent = "You should enter prompt";
+            return;
+        }
+        document.querySelector('.chat-input').style.display = 'none';
+        errorMsg.textContent = "";
+        let newMsg = document.createElement('div');
+        let linebreak = document.createElement('br');
+        newMsg.textContent = chatMsg.value;
+        newMsg.classList.add("my-msg");
+        chatLogs.appendChild(newMsg);
+        chatLogs.appendChild(linebreak);
+        chatLogs.appendChild(loadingMsg);
+
+        chatMsg.value = "";
+        // Call the Python function with the message as input
+        send_message(newMsg.textContent);
     });
+    function type_message(message) {
+        let i = 0;
+        let aiMsg = document.createElement('div');
+        let question = document.createElement('p');
+        aiMsg.classList.add("ai-msg");
+        question.classList.add("questions");
+        chatLogs.appendChild(aiMsg);
+        let typing = setInterval(function() {
+            if (i < message.length) {
+                aiMsg.textContent = message[0];
+                question.textContent = "testing ";
+                chatLogs.appendChild(question);
+                document.querySelector('.chat-input').style.display = 'block';
+                i++;
+            } else {
+                clearInterval(typing);
+            }
+        }, 10);
+    }
 
     $(function ($) {
         /* Here's where you'd do things on page load. */
     });
+
 }
+
