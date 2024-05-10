84812e84a4a0d4f5f0341a2a4dda86269b45bc1f
Yasir Ibrahim <yasir.ibrahim@gmail.com>
2024-05-10 12:13:52 +0000

    Fixed ai template rendering issue

--- guidedrubric/static/js/src/lms.js
+++ guidedrubric/static/js/src/lms.js
@@ -11,13 +11,13 @@ function GuidedRubricXBlock(runtime, element) {
     var is_attempted_phase_successful = null;
 
     // loader
-    let loadingMsg = document.createElement('div');
-    var overlay = document.createElement("div");
-    var loader = document.createElement("div");
-    overlay.setAttribute("class", "overlay");
-    overlay.setAttribute("id", "overlay-loader");
-    loader.setAttribute("class", "lds-dual-ring");
-    overlay.appendChild(loader);
+    // let loadingMsg = document.createElement('div');
+    // var overlay = document.createElement("div");
+    // var loader = document.createElement("div");
+    // overlay.setAttribute("class", "overlay");
+    // overlay.setAttribute("id", "overlay-loader");
+    // loader.setAttribute("class", "lds-dual-ring");
+    // overlay.appendChild(loader);
     // loadingMsg.classList.add("loading");
     // loadingMsg.appendChild(loader);
 
@@ -53,7 +53,12 @@ function GuidedRubricXBlock(runtime, element) {
             alert('You have reached to the end of excercise')
             return
         }
-        $('#chat_input_loader').css('display', '')
+        // $('').css('display', '')
+        let aiMsgs = document.querySelectorAll('.ai-msg');
+        let aiMsg = aiMsgs[aiMsgs.length - 1];
+        let loaders = document.querySelectorAll('.lds-dual-ring');
+        let loader = loaders[loaders.length - 1];
+        loader.style.display = 'block';
         // if (message != "skip")
         // {
         //     $('#chat_input_loader').css('display', '')
@@ -65,13 +70,22 @@ function GuidedRubricXBlock(runtime, element) {
             contentType:"application/json; charset=utf-8",
             dataType: "json",
         }).done(function(response) {
-            $('#chat_input_loader').css('display', 'none')
+            loader.style.display = 'none';
+            aiMsg.style.setProperty('display', 'flex', 'important');
+            // $('#chat_input_loader').css('display', 'none')
             if (response.result === 'success') {
-                if (response.response[1]){
+                if (response.response[1] == "Success" && response.response[2]) {
+                    type_message(response.response);
+                    keep_user_response(message, $('#last_attempted_phase_id'), response.response[0], response.response[2])
                     //var statusElements = document.getElementsByClassName('status');
                     //statusElements[statusElements.length - 1].textContent = "Status: " + response.response[1];
+                } 
+                else if (message == "skip" && response.response[2]) {
+                    keep_user_response(message, $('#last_attempted_phase_id'), response.response[0], response.response[2])
+                } else {
+                    type_message(response.response);
                 }
-                chatLogs.removeChild(loadingMsg);
+                // chatLogs.removeChild(loadingMsg);
                 let completion_token = response.response_metadata.completion_token
                 $('#completion_token').val(completion_token)
                 attempted_phase_is_last = response.response_metadata['attempted_phase_is_last']
@@ -79,47 +93,48 @@ function GuidedRubricXBlock(runtime, element) {
                 var permitted_status_for_showing_new_ques = [true, 'skip']
                 var res_is_attempted_phase_successful = response.response_metadata['is_attempted_phase_successful']
                 is_attempted_phase_successful = response.response_metadata['is_attempted_phase_successful']
-                if (permitted_status_for_typing_ai_msg.includes(res_is_attempted_phase_successful))
-                {
 
-                    //keep_user_response(message, $('#last_attempted_phase_id'), response.response[0], response.response_metadata['attempted_phase_question'])
-                    // type_message(response.response);
-                    if (response.response_metadata['attempted_phase_is_last'] == true && (res_is_attempted_phase_successful == true || res_is_attempted_phase_successful == 'skip'))
-                    {
-                        keep_user_response(message, $('#last_attempted_phase_id'), response.response[0], response.response_metadata['attempted_phase_question'])
-                        type_message(response.response);
-                        close_prompt(response.response)
-                    }
-                    else if (response.response_metadata['attempted_phase_is_last'] == true && res_is_attempted_phase_successful == false)
-                    {
-                        type_message(response.response);
-                        //hide_prompt()
-                    }
-                    else if (response.response_metadata['attempted_phase_is_last'] == false && permitted_status_for_showing_new_ques.includes(res_is_attempted_phase_successful))
-                    {
-                        keep_user_response(message, $('#last_attempted_phase_id'), response.response[0], response.response_metadata['attempted_phase_question'])
-                        type_message(response.response);
-                        hide_prompt();
-                        update_prompt_for_new_question(response.response[2]);
-                    }
-                    else if (response.response[1] == 'Fail' && !response.response_metadata['is_attempted_phase_successful'])
-                    {
-                        type_message(response.response);
-                    }
-                }
-                else if (response.response_metadata['next_phase_id'] != null)
-                {
-                    type_message(response.response);  
-                }
+                // if (permitted_status_for_typing_ai_msg.includes(res_is_attempted_phase_successful))
+                // {
 
-                else if (response.response_metadata['next_phase_id'] == null)
-                {
-                    alert('You have reached to the end of excercise')
-                }
-                //type_message(response.response);
+                //     //keep_user_response(message, $('#last_attempted_phase_id'), response.response[0], response.response_metadata['attempted_phase_question'])
+                //     // type_message(response.response);
+                //     if (response.response_metadata['attempted_phase_is_last'] == true && (res_is_attempted_phase_successful == true || res_is_attempted_phase_successful == 'skip'))
+                //     {
+                //         keep_user_response(message, $('#last_attempted_phase_id'), response.response[0], response.response_metadata['attempted_phase_question'])
+                //         type_message(response.response);
+                //         close_prompt(response.response)
+                //     }
+                //     else if (response.response_metadata['attempted_phase_is_last'] == true && res_is_attempted_phase_successful == false)
+                //     {
+                //         type_message(response.response);
+                //         //hide_prompt()
+                //     }
+                //     else if (response.response_metadata['attempted_phase_is_last'] == false && permitted_status_for_showing_new_ques.includes(res_is_attempted_phase_successful))
+                //     {
+                //         keep_user_response(message, $('#last_attempted_phase_id'), response.response[0], response.response_metadata['attempted_phase_question'])
+                //         type_message(response.response);
+                //         hide_prompt();
+                //         update_prompt_for_new_question(response.response[2]);
+                //     }
+                //     else if (response.response[1] == 'Fail' && !response.response_metadata['is_attempted_phase_successful'])
+                //     {
+                //         type_message(response.response);
+                //     }
+                // }
+                // else if (response.response_metadata['next_phase_id'] != null)
+                // {
+                //     type_message(response.response);  
+                // }
+
+                // else if (response.response_metadata['next_phase_id'] == null)
+                // {
+                //     alert('You have reached to the end of excercise')
+                // }
+                // //type_message(response.response);
             } else {
                 // alert("You've reached the end of the exercise. Hope you learned something!");
-                chatLogs.removeChild(loadingMsg);
+                // chatLogs.removeChild(loadingMsg);
                 document.querySelector('.chat-input').style.display = 'block';
             }
         }).fail(function(error) {
@@ -130,29 +145,23 @@ function GuidedRubricXBlock(runtime, element) {
     if (sendBtn != null)
     {
         sendBtn.addEventListener('click', function() {
-            //let status = document.createElement('div');
-            let chatMsg = document.getElementById('chat-msg');
-            if (!chatMsg.value.trim()) {
-                // errorMsg.textContent = "You should enter prompt";
-                // return;
+            // let chatMsg = document.getElementById('chat-msg');
+            let chatMsgs = document.querySelectorAll('.chat-msg');
+            let lastChatMsg = chatMsgs[chatMsgs.length - 1];
+
+            if (!lastChatMsg.value.trim()) {
                 alert("You should enter prompt")
                 return;
             }
-            //document.querySelector('.chat-input').style.display = 'none';
-            errorMsg.textContent = "";
-            var chat_message = chatMsg.value;
-            let newMsg = document.createElement('div');
-            //newMsg.textContent = "Your Answer: " + chatMsg.value;
-            newMsg.classList.add("my-msg");
-            //status.classList.add("status");
-            //status.textContent = "Status: Pending";
-            chatLogs.appendChild(newMsg);
-            //chatLogs.appendChild(status)
-            chatLogs.appendChild(loadingMsg);
+            console.log(lastChatMsg);
+            // errorMsg.textContent = "";
+            var chat_message = lastChatMsg.value;
+            // let newMsg = document.createElement('div');
+            // newMsg.classList.add("my-msg");
+            // chatLogs.appendChild(newMsg);
+            // chatLogs.appendChild(loadingMsg);
 
             last_attempted_phase_id = $('#last_attempted_phase_id').val()
-
-            //chatMsg.value = "";
             $('.micro-ai-btn-container').css('display', 'none')
             send_message(chat_message);
         });
@@ -160,18 +169,18 @@ function GuidedRubricXBlock(runtime, element) {
     if (skipBtn != null)
     {
         skipBtn.addEventListener('click', function() {
-        var status = document.createElement('div');
-        //document.querySelector('.chat-input').style.display = 'none';
-        errorMsg.textContent = "";
-        let newMsg = document.createElement('div');
-        //newMsg.textContent = "Your Answer: " + chatMsg.value;
-        //status.textContent = "Status: Skip";
-        newMsg.classList.add("my-msg");
-        //chatLogs.appendChild(newMsg);
-        //chatLogs.appendChild(status)
-        chatLogs.appendChild(loadingMsg);
-        chatMsg.value = "";
-        send_message("skip");
+            let chatMsgs = document.querySelectorAll('.chat-msg');
+            let aiMsgs = document.querySelectorAll('.ai-msg');
+            let lastChatMsg = chatMsgs[chatMsgs.length - 1];
+            let aiMsg = aiMsgs[aiMsgs.length - 1].querySelector('p');
+            console.log(aiMsg);
+            // errorMsg.textContent = "";
+            // let newMsg = document.createElement('div');
+            // newMsg.classList.add("my-msg");
+            // chatLogs.appendChild(loadingMsg);
+            lastChatMsg.value = "skip";
+            aiMsg.textContent = "None";
+            send_message("skip");
         });
     }
 
@@ -183,42 +192,40 @@ function GuidedRubricXBlock(runtime, element) {
         //         </div>`
         // $('#chat-logs').append(user_response_div)
 
-        if (user_input !== 'skip'){
+        // if (user_input !== 'skip'){
 
-        $('.recent-ai-msg').removeClass('recent-ai-msg')
-        var user_response_div = `<div id="chat-history">
+        $('.recent-ai-msg').removeClass('recent-ai-msg');
+        var user_response_div = `<div id="chat-logs">
         <p class="questions">`+attempted_phase_question+`</p>
+            </div>
+
+        <div class="chat-input" style="display: block;" id="prompt-with-loader">
+            <div id ="chat_input_loader" class="lds-dual-ring" style="display:none"></div><span id="error-msg"></span>
+            <textarea id="chat-msg" class="chat-msg" placeholder="Enter Prompt ... " rows="24" cols="230"></textarea>
         </div>
-        
-        <div class="chat-input" style="display: block;" id="prompt-with-loader-`+phase_id+`">
-            <textarea id="chat-msg-`+phase_id+`" rows="24" cols="230" disabled>`+user_input+`</textarea>
-        </div>
-        <div id="ai-msg-`+phase_id+`" class="ai-msg recent-ai-msg"><div class="icon-bar">
+        <div id="ai-msg" class="ai-msg recent-ai-msg"><div class="icon-bar">
             🤖
-        </div><p>`+ai_response+`</p></div>`
-        $('#chatbox-history').append(user_response_div)
+        </div><p></p></div>`
+        $('#chatbox-history').append(user_response_div);
 
-    }
-        else
-        {
-        $('.recent-ai-msg').removeClass('recent-ai-msg')
-        var user_response_div = `<div id="chat-history">
-        <p class="questions">`+attempted_phase_question+`</p>
-        </div>
+    // }
+    //     else
+    //     {
+    //     $('.recent-ai-msg').removeClass('recent-ai-msg');
+    //     var user_response_div = `<div id="chat-history">
+    //     <p class="questions">`+attempted_phase_question+`</p>
+    //     </div>
 
-        <div class="chat-input" style="display: block;" id="prompt-with-loader-`+phase_id+`">
-            <textarea id="chat-msg-`+phase_id+`" rows="24" cols="230" disabled>`+user_input+`</textarea>
-        </div>
-        <div id="ai-msg-`+phase_id+`" class="ai-msg recent-ai-msg">`+ai_response+`</div>`
-        $('#chatbox-history').append(user_response_div)
-        }
+    //     <div class="chat-input" style="display: block;" id="prompt-with-loader-`+phase_id+`">
+    //         <textarea id="chat-msg-`+phase_id+`" rows="24" cols="230" disabled>`+user_input+`</textarea>
+    //     </div>
+    //     <div id="ai-msg-`+phase_id+`" class="ai-msg recent-ai-msg">`+ai_response+`</div>`
+    //     $('#chatbox-history').append(user_response_div);
+    //     }
     }
 
     function update_prompt_for_new_question(question)
     {
-        // $('#chatbox-history #chat-logs').remove()
-        // $('#chatbox-history #prompt-with-loader').remove()
-        // $('#chatbox-history #ai-msg').remove()
 
         $('#chat-logs').remove()
         $('#prompt-with-loader').remove()
@@ -235,14 +242,12 @@ function GuidedRubricXBlock(runtime, element) {
         <div id="ai-msg" class="ai-msg recent-ai-msg"><div class="icon-bar">
             🤖
         </div><p></p></div>`
-        // $('.chatbox').append(new_prompt_div)
         $(new_prompt_div).insertBefore('.micro-ai-btn-primary.micro-ai-btn-container')
 
 
         //
         $('#chat-logs p').text(question)
         $('#chat-msg').val("")
-        // $('#ai-msg').text("")
         $('.recent-ai-msg').removeClass('recent-ai-msg')
         $('#ai-msg').addClass('recent-ai-msg')
         $('#chat-logs').css('display', '')
@@ -358,29 +363,23 @@ function GuidedRubricXBlock(runtime, element) {
 
 
     function type_message(data) {
-        // $('.previous-ai-msg').remove();
-        // $('.recent-ai-msg').textContent = ""
-        $('.recent-ai-msg p').empty();
+        var pElements = document.querySelectorAll('.ai-msg p');
+        let pElement = pElements[pElements.length - 1];
+
+        // $('.recent-ai-msg p').empty();
+        // aiMsg = pElement.textContent
 
 
         chunks = data[4]
-        let aiMsg = $('.recent-ai-msg').find('p');
-        //let question = document.createElement('p');
-        //question.classList.add("questions");
-        // let aiMsg = document.createElement('div');
-        // let question = document.createElement('p');
-        // aiMsg.classList.add("ai-msg");
-        // chatLogs.appendChild(aiMsg);
+        // let aiMsg = $('.recent-ai-msg').find('p');
     
         // This function will be called recursively with a delay to simulate streaming
         function displayNextChunk(index) {
             if (index < chunks.length) {
                 // Append the current chunk to the aiMsg element
-                let current_text = aiMsg.text()
-                let new_text = current_text +  chunks[index];
-                //aiMsg.textContent += chunks[index];
-                //$('.chat-input').css('display', 'none');
-                aiMsg.text(new_text)
+                let current_text = pElement.textContent
+                let new_text = current_text + chunks[index];
+                pElement.textContent = new_text; 
                 
                 // Call this function again for the next chunk after a short delay
                 setTimeout(() => displayNextChunk(index + 1), 100); // Adjust delay as needed
@@ -389,9 +388,6 @@ function GuidedRubricXBlock(runtime, element) {
                 if (data[1] == 'Success' || data[1]==null)
                 {
                     if (data[2]){
-                        //question.textContent = data[2];
-                        //chatLogs.appendChild(question);
-                        //$('#chat-logs p').text(data[2])
                         $('#send-btn').text(data[3])
                     }
                     if (data[2] === null) {
@@ -401,8 +397,6 @@ function GuidedRubricXBlock(runtime, element) {
                     }
                 }
                 if (data[3] == null){
-                    //$('.micro-ai-btn-container').css('display', 'none');
-                    //$('.chat-input').css('display', 'none');
                     let p = document.createElement('p');
                     p.classList.add('notification-btm');
                     p.textContent = data[5];
